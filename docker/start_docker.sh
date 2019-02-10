d87bd9a02ceca418cb9a52f516a2e76befec3f9f
alina <alina_ivchenkova@bk.ru>
2019-02-11 01:01:25 +0300

    docker_homework start

---
+++ docker/start_docker.sh
@@ -0,0 +1,11 @@
+#если необходимо, указать другой порт
+port=12345
+
+#!/bin/bash
+#создаем образ файла из Dockerfile:
+sudo docker build -t pg_alina_db .
+
+#запускаем новый контейнер
+sudo docker run -d -p $port:5432 --name pg_alina_db pg_alina_db
+
+
